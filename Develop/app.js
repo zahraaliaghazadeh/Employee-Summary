a61b8c5f3f5fab4d671c8ab9a79fe02c2be62b6c
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-04-14 16:14:27 -0700

    fixed the overwriting bug

--- Develop/app.js
+++ Develop/app.js
@@ -66,7 +66,7 @@ const inputArr = [
         type: "list",
         message: "Select the type of employee:",
         name: "employeeType",
-        choices: ["Intern","Engineer","Manager"]
+        choices: ["Intern", "Engineer", "Manager"]
     },
     // name for all employees 
     {
@@ -88,21 +88,21 @@ const inputArr = [
     },
     //school for intern only 
     {
-        type: "input", 
+        type: "input",
         message: "Enter the employee's school",
         name: "school",
         when: (response) => response.employeeType === 'Intern'
     },
     //engineer only - github username 
     {
-        type: "input", 
+        type: "input",
         message: "Enter the employee's GitHub username",
         name: "github",
         when: (response) => response.employeeType === 'Engineer'
     },
     //manager only - office number 
     {
-        type: "input", 
+        type: "input",
         message: "Enter the employee's office number",
         name: "officeNum",
         when: (response) => response.employeeType === 'Manager'
@@ -112,69 +112,71 @@ const inputArr = [
         type: "list",
         message: "Would you like to enter another employee?",
         name: "anotherEmployee",
-        choices: ["Yes","No"]
+        choices: ["Yes", "No"]
     }
-  ]
-
+]
+// declare the variable outside the function
+let employeeArr = [];
 //handles inquirer prompts & makes calls to render and writefile
 function prompt() {
     inquirer
-    //use prompt array 
-    .prompt(inputArr)
-    .then(function(response) {
-        //make an employee object based on which type of employee the user selected
-        let employee = null; 
-        if(response.employeeType === "Intern") {
-            //intern constructor takes name, id, email, and school 
-            employee = new Intern(response.name,response.id,response.email,response.school); 
-        }
-        else if(response.employeeType === "Engineer") {
-            //engineer constructor takes name, id, email, and github username 
-            employee = new Engineer(response.name,response.id,response.email,response.github); 
-        }
-        else {
-            //manager constructor takes name, id, email, and office number 
-            employee = new Manager(response.name,response.id,response.email,response.officeNum); 
-        }
-        employeeArr =[];
-        //add employee to array 
-        employeeArr.push(employee); 
+        //use prompt array 
+        .prompt(inputArr)
+        .then(function (response) {
+            //make an employee object based on which type of employee the user selected
+            let employee = null;
+            if (response.employeeType === "Intern") {
+                //intern constructor takes name, id, email, and school 
+                employee = new Intern(response.name, response.id, response.email, response.school);
+            }
+            else if (response.employeeType === "Engineer") {
+                //engineer constructor takes name, id, email, and github username 
+                employee = new Engineer(response.name, response.id, response.email, response.github);
+            }
+            else {
+                //manager constructor takes name, id, email, and office number 
+                employee = new Manager(response.name, response.id, response.email, response.officeNum);
+            }
 
-        //if the user selected that they want to enter another employee, run function again 
-        if(response.anotherEmployee === "Yes") {
-            prompt(); 
-        }
-        //otherwise, we are ready to write to the file 
-        else {
-            try {
-                //pass the array of employees to render to get the html 
-                //then pass that return string to the write file function 
-                writeFile(render(employeeArr)); 
-            } catch(error) {
-                console.error(error);
-            } 
-        }
-    }); 
+
+            //add employee to array 
+            employeeArr.push(employee);
+
+            //if the user selected that they want to enter another employee, run function again 
+            if (response.anotherEmployee === "Yes") {
+                prompt();
+            }
+            //otherwise, we are ready to write to the file 
+            else {
+                try {
+                    //pass the array of employees to render to get the html 
+                    //then pass that return string to the write file function 
+                    writeFile(render(employeeArr));
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        });
 }
 
 //writes the employee data to a file 
-function writeFile(fileData) { 
+function writeFile(fileData) {
     //check if directory exists. if not, create it 
-    if (!fs.existsSync(OUTPUT_DIR)){
+    if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR);
     }
-    
+
     ///write to file 
-    fs.writeFile(outputPath,render(employeeArr),function(error) {
-        if(error) {
-            console.log(error); 
+    fs.writeFile(outputPath, render(employeeArr), function (error) {
+        if (error) {
+            console.log(error);
         }
         else {
             console.log("html file is created in the output folder")
         }
-    }); 
+    });
 }
 
 //run the prompt 
-prompt(); 
+prompt();
 
